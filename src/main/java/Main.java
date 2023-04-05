af96f40866a0349ae57f63ce5ed41a3a6834059c
mrtuxa <mrtuxa@leipzig.freifunk.net>
2023-04-05 11:08:31 +0200

    respond to multiple pings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,36 +6,52 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-      // You can use print statements as follows for debugging, they'll be visible when running tests.
-      System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
 
-      //  Uncomment this block to pass the first stage
-      ServerSocket serverSocket = null;
-      Socket clientSocket = null;
-      int port = 6379;
-      try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+        // You can use print statements as follows for debugging, they'll be visible
+        // when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-          BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-          PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
+        //  Uncomment this block to pass the first stage
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
-          printWriter.println("+PONG\r");
-          printWriter.flush();
+            // Getting input and output streams from clientSocket
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
 
-      } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-      } finally {
-          try {
-              if (clientSocket != null) {
-                  clientSocket.close();
-              }
-          } catch (IOException e) {
-              System.out.println("IOException: " + e.getMessage());
-          }
-      }
-  }
+            String stringStart = "+";
+            String responseEnd = "\r\n";
+            String pingResponse = "PONG";
+
+            // Responding to client input
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.equalsIgnoreCase("ping")) {
+                    out.print(stringStart + pingResponse + responseEnd);
+                    out.flush();
+                }
+            }
+            clientSocket.close();
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
 }
+
